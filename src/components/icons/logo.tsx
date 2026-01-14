39b255cb68b99f7e960510da89bf272b9486af8f
Md Toha <mdtoha2035@gmail.com>
2026-01-14 05:04:35 +0000

    see there is no logo fix this

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className, ...props }: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="https://storage.googleapis.com/prompt-images/image-1.png"
+      src="https://i.imgur.com/3fHkS7v.png"
       alt="BetterPlugins Hub Logo"
       width={100}
       height={100}
