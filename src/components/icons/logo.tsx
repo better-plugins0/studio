44afa27f25cb3324531b58d4d423895c09f43bc5
Md Toha <mdtoha2035@gmail.com>
2026-01-14 05:07:02 +0000

    https://i.ibb.co.com/tpqRdJzC/Chat-GPT-Image-Jan-13-2026-02-56-18-PM.jpg

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className, ...props }: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="https://i.imgur.com/3fHkS7v.png"
+      src="https://i.ibb.co/tpqRdJzC/Chat-GPT-Image-Jan-13-2026-02-56-18-PM.jpg"
       alt="BetterPlugins Hub Logo"
       width={100}
       height={100}
